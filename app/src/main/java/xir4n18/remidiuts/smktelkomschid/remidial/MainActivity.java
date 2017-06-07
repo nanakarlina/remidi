7bb2bf91fb84ff567389ec6af8f7846cfa3b76f1
nana karlina <widyastutikarlina@gmail.com>
2017-06-07 13:20:13 +0700

    nav complete

--- app/src/main/java/xir4n18/remidiuts/smktelkomschid/remidial/MainActivity.java
+++ app/src/main/java/xir4n18/remidiuts/smktelkomschid/remidial/MainActivity.java
@@ -1,13 +1,46 @@
 package xir4n18.remidiuts.smktelkomschid.remidial;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn1, btn2, btn3, btn4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.nav1);
+        btn2 = (Button) findViewById(R.id.nav2);
+        btn3 = (Button) findViewById(R.id.nav3);
+        btn4 = (Button) findViewById(R.id.nav4);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, swipe.class));
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, tab.class));
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, spinner.class));
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, drawer.class));
+            }
+        });
     }
 }
